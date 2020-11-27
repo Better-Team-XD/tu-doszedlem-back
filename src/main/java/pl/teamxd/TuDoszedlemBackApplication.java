4969a4c5a4ba2d8c8cc6645112fa6f79abb5f080
krzysztof <misiek.nalepa@wp.pl>
2020-11-27 23:35:02 +0100

    init

---
+++ src/main/java/pl/teamxd/TuDoszedlemBackApplication.java
@@ -0,0 +1,13 @@
+package pl.teamxd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuDoszedlemBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuDoszedlemBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/teamxd/TuDoszedlemBackApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.teamxd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuDoszedlemBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
