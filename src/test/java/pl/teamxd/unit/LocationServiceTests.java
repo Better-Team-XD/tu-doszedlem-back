1b747662ba8879b4362b481312227db5b1eb9dee
Jakub Solecki <jakub.solecki@onet.com.pl>
2020-11-28 03:23:10 +0100

    added junit to pom.xml

---
+++ src/test/java/pl/teamxd/unit/LocationServiceTests.java
@@ -0,0 +1,10 @@
+package pl.teamxd.unit;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LocationServiceTests {
+
+}
