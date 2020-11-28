ab92dfaf7eb3752d957679ab3108043edc4c2e91
krzysztof <misiek.nalepa@wp.pl>
2020-11-28 02:30:32 +0100

    Security setup

---
+++ src/main/java/pl/teamxd/model/security/MyUserDetails.java
@@ -0,0 +1,60 @@
+package pl.teamxd.model.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import pl.teamxd.model.entity.MyUser;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class MyUserDetails implements UserDetails {
+    private final String username;
+    private final String password;
+    private final List<GrantedAuthority> authorities;
+
+
+    public MyUserDetails(MyUser user) {
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.authorities = Collections.singletonList(new SimpleGrantedAuthority(user.getRole().toString()));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
